eb12c58867d684336322a39031c03dbaaa270737
Corentin <corentintitboux@gmail.com>
2022-03-22 15:47:41 +0100

    Modif Quotient_familial.java

--- Model/Quotient_familial.java
+++ Model/Quotient_familial.java
@@ -6,7 +6,9 @@ public class Quotient_familial {
     /**
      * Default constructor
      */
-    public Quotient_familial(Parent ) {
+    public Quotient_familial(Parent p) {
+        quotient = CalculQuotient(p);
+        total = CalculTotal(quotient);
     }
 
     /**
@@ -14,5 +16,30 @@ public class Quotient_familial {
      */
     private int total;
 
+    /**
+     * 
+     */
+    private int quotient;
 
+    /**
+     * 
+     */
+    private int CalculQuotient(Parent p){
+        int q = ((1/12)*p.getSalaire())/(1+p.getEnfants().size());
+        if(q>100){
+            q = 100;
+        }
+        return q;
+    }
+
+    /**
+     * 
+     */
+    private int CalculTotal(int quotient){
+        return 1+(quotient/25);
+    }
+
+    public int getTotal(){
+        return total;
+    }
 }
