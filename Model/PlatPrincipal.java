ae3b4d55a53c2c2f199561fd08ae27f67f1d1bd7
AlexWlk <alex.wolak@orange.fr>
2022-03-22 15:22:13 +0100

    Construc GET SET PltPrincipal

--- Model/PlatPrincipal.java
+++ Model/PlatPrincipal.java
@@ -5,26 +5,53 @@ import java.util.*;
  * 
  */
 public class PlatPrincipal {
-
-    /**
-     * Default constructor
-     */
-    public PlatPrincipal() {
-    }
-
+    
     /**
      * 
      */
     private String nom_plat;
 
     /**
-     * 
+     * Default constructor
      */
-    private String aliments;
+    
+    public PlatPrincipal(String nom_plat, String aliments, Set<Aliments> liste_a) {
+        this.nom_plat = nom_plat;
+        this.aliments = aliments;
+        this.liste_a = liste_a;
+    }
 
     /**
      * 
      */
+    private String aliments;
     private Set<Aliments> liste_a;
+    
+    /**
+     * 
+     */
+    public String getNom_plat() {
+        return nom_plat;
+    }
+
+    public void setNom_plat(String nom_plat) {
+        this.nom_plat = nom_plat;
+    }
+
+    public String getAliments() {
+        return aliments;
+    }
+
+    public void setAliments(String aliments) {
+        this.aliments = aliments;
+    }
+
+    public Set<Aliments> getListe_a() {
+        return liste_a;
+    }
+
+    public void setListe_a(Set<Aliments> liste_a) {
+        this.liste_a = liste_a;
+    }
 
 }
