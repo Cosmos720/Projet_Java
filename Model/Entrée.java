e0a7a17f2c0999637a5f63a8af2493d84357f295
AlexWlk <alex.wolak@orange.fr>
2022-03-22 15:26:26 +0100

    Entree GET SET CONST

--- Model/Entrée.java
+++ Model/Entrée.java
@@ -9,7 +9,11 @@ public class Entrée {
     /**
      * Default constructor
      */
-    public Entrée() {
+    
+    public Entrée(String nom_entrée, String aliments, Set<Aliments> liste_a) {
+        this.nom_entrée = nom_entrée;
+        this.aliments = aliments;
+        this.liste_a = liste_a;
     }
 
     /**
@@ -29,5 +33,47 @@ public class Entrée {
      * 
      */
     private Set<Aliments> liste_a;
+
+
+
+
+    public String getNom_entrée() {
+        return nom_entrée;
+    }
+
+
+
+
+    public void setNom_entrée(String nom_entrée) {
+        this.nom_entrée = nom_entrée;
+    }
+
+
+
+
+    public String getAliments() {
+        return aliments;
+    }
+
+
+
+
+    public void setAliments(String aliments) {
+        this.aliments = aliments;
+    }
+
+
+
+
+    public Set<Aliments> getListe_a() {
+        return liste_a;
+    }
+
+
+
+
+    public void setListe_a(Set<Aliments> liste_a) {
+        this.liste_a = liste_a;
+    }
 
 }
