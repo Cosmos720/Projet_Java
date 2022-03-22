1c975e468081097bb11968c5c11ad87eaaf526a9
AlexWlk <alex.wolak@orange.fr>
2022-03-22 15:28:30 +0100

    3 MENU GET SET CONSTRUCT

--- Model/MenuVegetarien.java
+++ Model/MenuVegetarien.java
@@ -6,10 +6,14 @@ import java.util.*;
  */
 public class MenuVegetarien extends Repas {
 
+    
     /**
      * Default constructor
      */
-    public MenuVegetarien() {
+    
+    public MenuVegetarien(int tarif_repas, int quantité_repas, String date_repas, Set<Enfant> enfants, Cantine cantine,
+            Set<Reservation> resa, Dessert dessert, Entrée entree, PlatPrincipal plat) {
+        super(tarif_repas, quantité_repas, date_repas, enfants, cantine, resa, dessert, entree, plat);
     }
 
 }
