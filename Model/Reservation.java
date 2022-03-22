68aaad6bf6489cd88f1bfdaaed81be273c37cc7c
AlexWlk <alex.wolak@orange.fr>
2022-03-22 15:24:58 +0100

    RESA GET SET CONST

--- Model/Reservation.java
+++ Model/Reservation.java
@@ -9,13 +9,22 @@ public class Reservation {
     /**
      * Default constructor
      */
-    public Reservation() {
+    
+    public Reservation(String nom_associé, String date_reservation, int prix, boolean absence, Compte compte,
+            Repas repas) {
+        this.nom_associé = nom_associé;
+        this.date_reservation = date_reservation;
+        this.prix = prix;
+        this.absence = absence;
+        this.compte = compte;
+        this.repas = repas;
     }
 
     /**
      * 
      */
     private String nom_associé;
+
 
     /**
      * 
@@ -32,9 +41,6 @@ public class Reservation {
      */
     private boolean absence;
 
-
-
-
     /**
      * 
      */
@@ -44,5 +50,53 @@ public class Reservation {
      * 
      */
     private Repas repas;
+
+    public String getNom_associé() {
+        return nom_associé;
+    }
+
+    public void setNom_associé(String nom_associé) {
+        this.nom_associé = nom_associé;
+    }
+
+    public String getDate_reservation() {
+        return date_reservation;
+    }
+
+    public void setDate_reservation(String date_reservation) {
+        this.date_reservation = date_reservation;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public boolean isAbsence() {
+        return absence;
+    }
+
+    public void setAbsence(boolean absence) {
+        this.absence = absence;
+    }
+
+    public Compte getCompte() {
+        return compte;
+    }
+
+    public void setCompte(Compte compte) {
+        this.compte = compte;
+    }
+
+    public Repas getRepas() {
+        return repas;
+    }
+
+    public void setRepas(Repas repas) {
+        this.repas = repas;
+    }
 
 }
