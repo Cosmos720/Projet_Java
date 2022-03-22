37e96121e594aaaeba03d8bb572071efe7fd61f6
Arm Skr <scorp-arm@hotmail.com>
2022-03-22 14:50:50 +0100

    Get et Constructeur Compte.java

--- Model/Compte.java
+++ Model/Compte.java
@@ -2,39 +2,72 @@
 import java.util.*;
 
 /**
- * 
+ *
  */
 public class Compte {
 
     /**
      * Default constructor
      */
-    public Compte() {
+    public Compte(int id, String mdp, int solde, Vector<Parent> user, Vector<Reservation> resa) {
+        this.id = id;
+        this.mdp = mdp;
+        this.solde = solde;
+        this.user = user;
+        this.resa = resa;
     }
 
-    /**
-     * 
-     */
-    private int identifiant;
 
     /**
-     * 
+     *
      */
-    private String mot_de_passe;
+    private int id;
 
     /**
-     * 
+     *
+     */
+    private String mdp;
+
+    /**
+     *
      */
     private int solde;
 
     /**
-     * 
+     *
      */
-    private Set<Parent> utilisateur;
+    private Vector<Parent> user;
 
     /**
-     * 
+     *
      */
-    private Set<Reservation> resa;
+    private Vector<Reservation> resa;
+
+    public int getid() {
+        return id;
+    }
+
+
+    public String getmdp() {
+        return mdp;
+    }
+
+    public int getSolde() {
+        return solde;
+    }
+
+    public Vector<Parent> getuser() {
+        return user;
+    }
+
+    public Vector<Reservation> getResa() {
+        return resa;
+    }
+
+    public void setResa(Vector<Reservation> resa) {
+        this.resa = resa;
+    }
+
+    
 
 }
