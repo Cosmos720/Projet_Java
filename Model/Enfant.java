2757d980ed33b4a77e4f43aefc885924ad1496a0
AlexWlk <alex.wolak@orange.fr>
2022-03-22 15:20:12 +0100

    Constructor get set enfant

--- Model/Enfant.java
+++ Model/Enfant.java
@@ -9,13 +9,19 @@ public class Enfant extends Personne {
     /**
      * Default constructor
      */
-    public Enfant() {
-    }
-
-    /**
-     * 
-     */
+    
     private String classe;
+
+    public Enfant(String nom, String prenom, int age, String sexe, String classe, int horaires_cours, String allergies,
+            Set<Parent> parents, Set<Repas> repas, Cantine cantine) {
+        super(nom, prenom, age, sexe);
+        this.classe = classe;
+        this.horaires_cours = horaires_cours;
+        this.allergies = allergies;
+        this.parents = parents;
+        this.repas = repas;
+        this.cantine = cantine;
+    }
 
     /**
      * 
@@ -41,5 +47,53 @@ public class Enfant extends Personne {
      * 
      */
     private Cantine cantine;
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public int getHoraires_cours() {
+        return horaires_cours;
+    }
+
+    public void setHoraires_cours(int horaires_cours) {
+        this.horaires_cours = horaires_cours;
+    }
+
+    public String getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
+
+    public Set<Parent> getParents() {
+        return parents;
+    }
+
+    public void setParents(Set<Parent> parents) {
+        this.parents = parents;
+    }
+
+    public Set<Repas> getRepas() {
+        return repas;
+    }
+
+    public void setRepas(Set<Repas> repas) {
+        this.repas = repas;
+    }
+
+    public Cantine getCantine() {
+        return cantine;
+    }
+
+    public void setCantine(Cantine cantine) {
+        this.cantine = cantine;
+    }
 
 }
