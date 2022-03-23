11d126e46796dae570d5d1132e92450a4061ffbb
unknown <73186172+HarlausG@users.noreply.github.com>
2022-03-23 09:33:22 +0100

    Creation de package

---
+++ Vue/GUI.java
@@ -0,0 +1,15 @@
+package Vue;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class GUI extends JFrame {
+
+    public GUI(){
+        this.setSize(800,800);
+        setLocationRelativeTo(null);
+        setResizable(true);
+        setVisible(true);
+    }
+}
