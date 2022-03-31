c204403bceaba506b927db8e3ae68a15eb26376d
unknown <73186172+HarlausG@users.noreply.github.com>
2022-03-31 17:32:07 +0200

    Ajout vue et controller creerEnfant

---
+++ Vue/pageCreerEnfant.java
@@ -0,0 +1,99 @@
+package Vue;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+import Controller.*;
+
+public class pageCreerEnfant extends JFrame {
+
+	
+	private JTextField textField_nom = new JTextField();
+	private JTextField textField_prenom = new JTextField();
+	private JTextField textField_sexe = new JTextField();
+	private JTextField textField_age = new JTextField();
+	private JTextField textField_classe = new JTextField();
+	
+	private JButton btn_enfant = new JButton("Creer Enfant");
+
+	private controleCreerEnfant controle;
+	
+	public pageCreerEnfant(controleCreerEnfant ctl) {
+		controle = ctl;
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400,400);
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new EmptyBorder(20, 50, 20, 50));
+		getContentPane().add(panel_1);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.Y_AXIS));
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new EmptyBorder(0, 0, 20, 0));
+		panel_1.add(panel_2);
+		panel_2.setLayout(new GridLayout(5, 2, 20, 10));
+		
+        // Ajout Nom
+		JLabel label_nom = new JLabel("Nom");
+		label_nom.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		panel_2.add(label_nom);
+		panel_2.add(textField_nom);
+		
+        // Ajout Prenom
+		JLabel label_prenom = new JLabel("Prenom");
+		label_prenom.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		panel_2.add(label_prenom);
+		panel_2.add(textField_prenom);
+		
+        // Ajout Sexe
+		JLabel label_sexe = new JLabel("Sexe");
+		label_sexe.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		panel_2.add(label_sexe);
+		panel_2.add(textField_sexe);
+		
+        // Ajout Age
+		JLabel label_age = new JLabel("Age");
+		label_age.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		panel_2.add(label_age);
+		panel_2.add(textField_age);
+		
+        // Ajout Classe
+		JLabel label_classe = new JLabel("Classe");
+		label_classe.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		panel_2.add(label_classe);
+		panel_2.add(textField_classe);
+		
+		
+		btn_enfant.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btn_enfant.setName("creer");
+		btn_enfant.addActionListener(controle);
+		panel_1.add(btn_enfant);
+		
+		setVisible(true);
+		
+	}
+
+	public String getNom(){
+        return this.textField_nom.getText();
+    }
+
+    public String getPrenom(){
+        return this.textField_prenom.getText();
+    }
+
+    public String getSexe(){
+        return this.textField_sexe.getText();
+    }
+
+    public String getAge(){
+        return this.textField_age.getText();
+    }
+
+    public String getClasse(){
+        return this.textField_classe.getText();
+    }
+
+}
