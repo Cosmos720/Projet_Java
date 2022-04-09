edacb2b1c8fd36e9009d76e8db35a458364e07c7
Cosmos720 <corentintitboux@gmail.com>
2022-04-09 15:33:16 +0200

    Modif Cantine.java constructeur & add in Vector

--- Model/Cantine.java
+++ Model/Cantine.java
@@ -11,11 +11,11 @@ public class Cantine {
     /**
      * Default constructor
      */
-    public Cantine(String nom_cantine, int capacité, Vector<Enfant> enfants, Vector<Compte> comptes) {
+    public Cantine(String nom_cantine, int capacité) {
             this.nom_cantine = nom_cantine;
             this.capacité = capacité;
-            this.enfants = enfants;
-            this.comptes = comptes;
+            this.enfants = new Vector<Enfant>(this.capacité, 0);
+            this.comptes = new Vector<Compte>();
         }
 
     /**
@@ -38,49 +38,51 @@ public class Cantine {
      */
     private Vector<Compte> comptes;
 
-
-    public Vector<Compte> getComptes() {
-        return comptes;
-    }
-
-
-
-    public void setComptes(Vector<Compte> comptes) {
-        this.comptes = comptes;
-    }
-
+    
 
 
     public String getNom_cantine() {
         return nom_cantine;
     }
 
-
-
     public void setNom_cantine(String nom_cantine) {
         this.nom_cantine = nom_cantine;
     }
 
-
-
     public int getCapacité() {
         return capacité;
     }
-
-
 
     public void setCapacité(int capacité) {
         this.capacité = capacité;
     }
 
 
+
+    public Vector<Compte> getComptes() {
+        return comptes;
+    }
+
+    public void setComptes(Vector<Compte> comptes) {
+        this.comptes = comptes;
+    }
+
+    public boolean addComptes(Compte c) {
+        return this.comptes.add(c);
+    }
+
+
+
     public Vector<Enfant> getEnfants() {
         return enfants;
     }
 
-
     public void setEnfants(Vector<Enfant> enfants) {
         this.enfants = enfants;
+    }
+
+    public boolean addEnfants(Enfant e) {
+        return this.enfants.add(e);
     }
 
 
