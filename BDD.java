bbccb20d7264153087026aab302b3b4f200d3507
Gauthier <73186172+HarlausG@users.noreply.github.com>
2022-04-11 21:26:55 +0200

    Calcule quotient BDD

--- BDD.java
+++ BDD.java
@@ -13,6 +13,7 @@ public class BDD {
         Arm.setParent(Armen);
         Enfant ArmJunior = new Enfant("Sulejmanovic", "Alexandre", 5, "Garcon", "Aucun", Arm);
         Armen.addEnfant(ArmJunior);
+        Arm.getuser().newQuotient();
 
         Compte Coco = new Compte("Coco", "Cosmos720", 1 , null, new Vector<Reservation>());
         Parent Corentin = new Parent("Boux", "Corentin", 42, "Femme", Coco, new Vector<Enfant>(), 1200);
@@ -21,6 +22,7 @@ public class BDD {
         Enfant CocoZelda = new Enfant("Boux", "Zelda", 9, "Femme", "Aucun", Coco);
         Corentin.addEnfant(CocoLink);
         Corentin.addEnfant(CocoZelda);
+        Coco.getuser().newQuotient();
 
         Compte Awk = new Compte("Awk", "awk", 2000 , null, new Vector<Reservation>());
         Parent Alexandre = new Parent("Wolak", "Alexandre", 42, "Homme", Awk, new Vector<Enfant>(), 3000);
@@ -29,6 +31,7 @@ public class BDD {
         Enfant Denis = new Enfant("Dupont", "Denis", 16, "Garcon", "Aucun", Awk);
         Alexandre.addEnfant(Kevin);
         Alexandre.addEnfant(Denis);
+        Awk.getuser().newQuotient();
 
         Compte Harlaus = new Compte("Harlaus", "azerty", 3000 , null, new Vector<Reservation>());
         Parent Gauthier = new Parent("Evraerd", "Gauthier", 24, "Homme", Harlaus, new Vector<Enfant>(), 8000);
@@ -37,7 +40,7 @@ public class BDD {
         Enfant Clara = new Enfant("Papillion", "Clara", 4, "Fille", "Aucun", Harlaus);
         Gauthier.addEnfant(Laura);
         Gauthier.addEnfant(Clara);
-
+        Harlaus.getuser().newQuotient();
 
         Cantine cantine = new Cantine("Cantine pour les Tipeu", 200);
 		cantine.addComptes(Arm);
